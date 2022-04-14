7888db77cd5871cc0afee3e1a19e209b436f75a2
TofikSaliba <tofik.saliba@gmail.com>
2022-04-14 18:52:48 +0300

    added ex 2.6 and 2.7

---
+++ Ex-2.7.js
@@ -0,0 +1,25 @@
+//! 2.7 Basic Math
+//? Your task is to create a function that does four basic .
+//? The function should take three arguments - operation(string/char), value1(number),
+//? value2(number).
+//? The function should return result of numbers after applying the chosen operation.
+//? Examples
+//? basicOp('+', 4, 7) // Output: 11
+//? basicOp('-', 15, 18) // Output: -3
+//? basicOp('*', 5, 5) // Output: 25
+//? basicOp('/', 49, 7) // Output: 7
+
+const BasicMath = (operation, num1, num2) => {
+  switch (operation) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+  }
+};
+
+console.log(BasicMath("*", 7, 5));
